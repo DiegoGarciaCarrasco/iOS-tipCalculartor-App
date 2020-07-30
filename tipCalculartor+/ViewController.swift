c7bf0baad00de848283607eb6b1eaad6e61b5515
Diego Garcia <diegoalonzo_prod@hotmail.com>
2020-07-30 01:16:31 -0700

    Initial Commit

---
+++ tipCalculartor+/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipCalculartor+
+//
+//  Created by Diego Garcia on 7/30/20.
+//  Copyright © 2020 GarcsTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
